324e05da4943833717b1eb4e1001e9d48d536a71
Nick0603 <b10303008@gmail.com>
2017-08-12 03:13:51 +0800

    基礎版js完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-$("#modalBtn").click(function(){
-    $('#myModalScratch').modal("show")
-});
+var minNumber = null;
+var maxNumber = null;
+var answer = null;
+
+function getRandom(min , max){
+	return Math.floor(Math.random()* (max-min+1))+min;
+}
+
+function initGame(){
+	var level = $(":checked").val();
+	if( level == "easy"){
+		minNumber = 0;
+		maxNumber = 100;
+	}else if( level == "normal"){
+		minNumber = 0;
+		maxNumber = 500;
+	}else{
+		minNumber = 0;
+		maxNumber = 1000;
+	}
+	answer = getRandom( minNumber+1 , maxNumber-1 );
+	updateView();
+}
+
+function updateView(){
+	$("#minNumberLabel").text(minNumber);
+	$("#maxNumberLabel").text(maxNumber);
+}
+
+function guess(){
+	var guessNumber = $("#guessInput").val();
+	if(guessNumber > answer){
+		maxNumber = guessNumber;
+	}else if(guessNumber < answer){
+		minNumber = guessNumber;
+	}else{
+		alert("勝利")
+	}
+	updateView();
+	clear();
+}
+
+function clear(){
+	$("#guessInput").val("");
+}
+
+$("#startGameBtn").click(initGame);
+$("#clearBtn").click(clear);
+$("#guessBtn").click(guess);
